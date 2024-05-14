2a73ebd5aa0107aa45fcaf31947b3d40fc6a4fd4
Greg Tate <gtatelive@users.noreply.github.com>
2024-05-14 15:53:10 -0500

    Update altparameterexample.py

--- altparameterexample.py
+++ altparameterexample.py
@@ -1,25 +1,19 @@
-
-#%%
-import argparse
-import pandas as pd
-import sys
-
 #%%
 # Simulate command line arguments using sys.argv
 # Replace these with actual arguments when running the script standalone
-# simulated_argv = [
-#     'script_name.py', 
-#     '--output','database', 
-#     '--db_host', 'localhost', 
-#     '--db_name', 'mydb', 
-#     '--db_user', 'user', 
-#     '--db_password', 'password'
-#     ]
 simulated_argv = [
     'script_name.py', 
-    '--output','csv', 
-    '--csv_path', 'somefile.csv'
+    '--output','database', 
+    '--server', 'server.domain.com', 
+    '--table',  'table', 
+    '--port', '1433', 
     ]
+# simulated_argv = [
+#     'script_name.py', 
+#     '--output','csv', 
+#     '--fileName', 'somefile.csv',
+#     '--filePath', 'c:\\temp\\'
+#     ]
 
 
 
@@ -32,14 +26,17 @@ parser = argparse.ArgumentParser(description="Export Data to Database or CSV")
 # Add Main Arguments
 parser.add_argument("--output", type=str, choices=['database', 'csv'], help="Choose output type: database or csv", required=True)
 
-# Sub-parameters for database
-parser.add_argument("--db_host", type=str, help="Database host address")
-parser.add_argument("--db_name", type=str, help="Name of the database")
-parser.add_argument("--db_user", type=str, help="Database user name")
-parser.add_argument("--db_password", type=str, help="Database password")
-
 # Sub-parameter for csv
-parser.add_argument("--csv_path", type=str, help="Path to save the CSV file")
+parser.add_argument("--fileName", type=str, help="Name of the CSV file")
+parser.add_argument("--filePath", type=str, help="Path to save the CSV file")
+
+
+# Sub-parameters for database
+parser.add_argument("--server", type=str, help="FQDN of Database Server")
+parser.add_argument("--table", type=str, help="Name of the database table")
+parser.add_argument("--port", type=str, help="Database port")
+
+
 
 
 # Temporarily replace sys.argv for testing purposes
@@ -54,17 +51,17 @@ sys.argv = original_argv
 df = pd.DataFrame({ 'A': ['foo', 'bar', 'baz'], 'B': ['one', 'two', 'three'] })
 
 if args.output == 'database':
-    if args.db_host is None or args.db_name is None or args.db_user is None or args.db_password is None:
+    if args.server is None or args.table is None or args.port is None:
         print("Database connection details are missing.")
     else:
-        print(f"Exporting data to database at {args.db_host}/{args.db_name} as user {args.db_user}")
+        print(f"Exporting data to database at {args.server}/{args.table} on port {args.port}")
         # Implement code to export dataframe df to specified database
         
 elif args.output == 'csv':
-    if args.csv_path is None:
+    if args.fileName is None:
         print("CSV file location is missing.")
     else:
-        print(f"Exporting data to CSV at {args.csv_path}")
+        print(f"Exporting data to CSV at {args.fileName}")
         # Implement code to export dataframe df to specified CSV path
 else:
     print("Invalid output type chosen.")
